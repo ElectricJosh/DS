db2116dd0019d693f435cf0d81aee23b7d3bf226
ElectricJoshy <joshbowen@btinternet.com>
2021-04-09 18:58:27 +0100

    Added Azure Test

---
+++ AzureTest/AzureTest/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Xaml.Controls;
+
+using Microsoft.Azure.Devices.Client;
+using System.Text;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AzureTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SendDeviceToCloudMessagesAsync();
+        }
+
+        static async void SendDeviceToCloudMessagesAsync()
+        {
+
+            string iotHubUri = "ElectricJosh.azure-devices.net";
+            string deviceKey = "q+LyxuvjoEnaEn4DVWSClfO0vX2p3F0mQwt4dbrXKbA=";
+            string deviceId = "AAs1qKYBsj2inp%2bha0RWjozirQkjYJDrAuk3WEaP1K7zpw%3d%3d";
+
+            var deviceClient = DeviceClient.Create(iotHubUri,
+                    AuthenticationMethodFactory.
+                        CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey),
+                    TransportType.Http1);
+
+            var str = "Hello, Cloud!";
+            var message = new Message(Encoding.UTF8.GetBytes(str));
+
+            message.ContentEncoding = "utf-8";
+            message.ContentType = "application/json";
+
+            await deviceClient.SendEventAsync(message);
+        }
+    }
+}
